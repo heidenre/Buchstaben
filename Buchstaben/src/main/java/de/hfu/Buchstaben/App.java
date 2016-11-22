26de8b58d9aa1f1c331d21064302a91d13c54256
Andreas Heidenreich <andreas.heidenreich@hs-furtwangen.de>
2016-11-22 17:10:31 +0100

    Erster Commit

---
+++ Buchstaben/src/main/java/de/hfu/Buchstaben/App.java
@@ -0,0 +1,18 @@
+package de.hfu.Buchstaben;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        
+        System.out.println(s.toUpperCase());
+        sc.close();
+    }
+}
