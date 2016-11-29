459edb5322eddaa12f51a71103276ad8162e3dc8
Andreas Heidenreich <andreas.heidenreich@hs-furtwangen.de>
2016-11-29 15:44:37 +0100

    Queue und Util hinzugefügt

--- Buchstaben/src/main/java/de/hfu/Buchstaben/App.java
+++ Buchstaben/src/main/java/de/hfu/Buchstaben/App.java
@@ -2,11 +2,18 @@ package de.hfu.Buchstaben;
 import java.util.Scanner;
 
 /**
- * Hello world!
- *
+ * 
+ * Grossbuchstaben_Konvertierer
+ * @author Andreas
+ * 
  */
 public class App 
 {
+	/**
+	 * 
+	 * Hauptprogramm
+	 * 
+	 */
     public static void main( String[] args )
     {
         Scanner sc = new Scanner(System.in);
